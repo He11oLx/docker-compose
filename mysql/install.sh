bfc9658e0a7b7968140910393e0c554bff1cdbe8
Lx <vip.gmail@qq.com>
2020-09-28 22:39:12 +0800

    fix wsl mysql initialize

---
+++ mysql/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir conf
+
+cat >./conf/my.cnf<<EOF
+# The MySQL  Server configuration file.
+# http://dev.mysql.com/doc/mysql/en/server-system-variables.html
+
+[mysqld]
+pid-file        = /var/run/mysqld/mysqld.pid
+socket          = /var/run/mysqld/mysqld.sock
+datadir         = /var/lib/mysql
+#log-error      = /var/log/mysql/error.log
+# By default we only accept connections from localhost
+#bind-address   = 127.0.0.1
+# Disabling symbolic-links is recommended to prevent assorted security risks
+symbolic-links=0
+EOF
+
+docker-compose up -d
